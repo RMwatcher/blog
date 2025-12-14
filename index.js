5ce906a9e651d8aa2c980e51266edf90cba6a12b
Richard Mora <llody777@gmail.com>
2025-12-13 23:25:07 -0800

    added express code to js file and ejs file

--- index.js
+++ index.js
@@ -4,11 +4,32 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+const userName = "";
+let subject = "";
+let content = "";
+
+function createPost (req, res, next) {
+    userName = req.body["uName"];
+    subject = req.body["subject"];
+    content = req.body["content"];
+    next();
+};
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.use(createPost);
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
+});
+
+app.get("/create", (req, res) => {
+    res.render("create.ejs");
+});
+
+app.post("/submit", (req, res) => {
+    console.log("Your post was submitted.")
 });
 
 app.listen(port, () => {
